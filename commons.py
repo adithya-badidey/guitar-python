cc1fad8f4ee31e0d5d84ac110fca6b103ba1203e
Adithya Badidey <bmadithya@gmail.com>
2023-01-29 15:54:43 -0700

    Intermediate commit

--- commons.py
+++ commons.py
@@ -114,6 +114,101 @@ def changeRootNote(noteset, index):
   newProg = noteset.prog[index:] + noteset.prog[:index]
   return NoteSet(newProg)
   
+natural_scale   = [0, 2, 4, 5, 7, 9, 11]
+# minor_melodic   = [0, 2, 3, 5, 7, 9, 10]
+# minor_harmonic  = [0, 2, 3, 5, 7, 8, 11]
+# major_harmonic  = [0, 2, 3, 5, 7, 8, 11]
+
+def parseScaleListWH(notesList):
+  res = [0]
+  for i in notesList:
+      halfs = i.count('H')
+      fulls = i.count('W')
+      res.append(res[-1] + halfs + fulls*2)
+  return res
+
+def parseScaleList(notesList, reference):
+  if 'W' in notesList[0] or 'H' in notesList[0]:
+      return parseScaleListWH(notesList)
+
+  res = []
+  for i in notesList:
+      i = i.replace(' ','')
+      flats = i.count('b')
+      sharps = i.count('#')
+      num = int(i.replace('b','').replace('#','')) - 1
+      res.append(reference[num] - flats + sharps)
+  res.sort()
+  return res
+
+class Scale:
+  def __init__(self, progression, name=None, reference = natural_scale): #reference is major
+      if isinstance(progression, list) or isinstance(progression, tuple):
+          if isinstance(progression[0], int):
+              if all(i in [0,1] for i in progression): #In case its a binary scale
+                  assert len(progression) == 12
+                  # progression should be something like [x1, x2 .. x12] 
+                  # where each xi in {0, 1}
+                  self.prog = [index for index, i in enumerate(progression) if i == 1]
+              else:
+                  self.prog = list(progression)
+          elif isinstance(progression[0], str):
+              self.prog = parseScaleList(progression, reference)
+          else:
+              raise Exception(f"Unknown dtype for progression[0]: {type(progression[0])}")
+      elif isinstance(progression, str):
+          self.prog = parseScaleList(progression.split(','), reference)
+      else:
+          raise Exception(f"Unknown dtype for progression: {type(progression)}")
+      self.name = name
+      self.aliases = []
+
+  def addAlias(self, alias):
+      self.aliases.append(alias)
+
+  def __str__(self):
+      # prog = [f"{i:>3}" for i in self.progression]
+      name = self.name or "Unnamed"
+      return f"{name:15}: {self.prog}"
+
+  def __getitem__(self, item):
+      return self.prog[item]
+  
+  def __len__(self):
+      return len(self.prog)
+
+  def mode(self, num=1, name=None):
+      assert num > 0 and num <= len(self.prog)
+      num -= 1
+      root = self.prog[num]
+      
+      prog = [i - root for i in self.prog[num:]] \
+          + [i - root + 12 for i in self.prog[:num]]
+      # print(prog)
+      return Scale(prog, name=name)
+  
+  def distance(self, other):
+      assert len(self) == len(other)
+      dist = 0
+      for i in range(len(self)):
+          if self[i] != other[i]:
+              dist += 1
+      return dist
+
+  def obtainRelativeName(self, other):
+      assert len(self) == len(other)
+      mods = []
+      for i in range(len(self)):
+          if self[i] != other[i]:
+              dist = self[i] - other[i]
+              if dist < 0:
+                  mod = 'b' * (-dist)
+              # elif dist == 0:
+              #     mod = '♮'
+              else:
+                  mod = '#' * dist
+              mods.append(f"{mod}{i+1}")
+      return other.name + " " + " ".join(mods)
 
 chords = {
   'maj':  [0,4,7],
--- readme.md
+++ readme.md
@@ -5,3 +5,14 @@ python3 -m venv .env
 source .env/bin/activate
 pip install --upgrade pip
 ```
+
+# Tasks
+[ ] Finish combinatorics
+[ ] Write a namer for chords
+[ ] Reverse namer for scales and chords
+[ ] 
+
+
+Wishlist:
+[ ] For any scale, visualize the patterns
+[ ] For any scale, find the chords
